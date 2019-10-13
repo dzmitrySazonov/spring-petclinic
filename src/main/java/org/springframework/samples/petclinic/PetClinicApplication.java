941da029c8b7985ecbf9a59cb499e58d2a6f9342
Dzmitry Sazonau <dzmitry_sazonau1@epam.com>
2019-10-14 01:05:17 +0300

    new commit

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -27,7 +27,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class PetClinicApplication {
-    private int field = 0;
+    private int field = 1;
 
     public static void main(String[] args) {
         SpringApplication.run(PetClinicApplication.class, args);
